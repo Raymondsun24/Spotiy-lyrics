8d920279613edb3640c8d318c9b97930ccc2b3b5
Yizhi <lycheesun@hotmail.com>
2020-05-31 15:31:13 -0400

    make it better

--- index.js
+++ index.js
@@ -12,17 +12,16 @@ app.get('/', (req,res)=>{
 });
 
 const my_client_id = '240cd0ccc20e40e087947ffa1c710b42';
-const redirect_uri = 'https://shrouded-escarpment-08729.herokuapp.com/login/redirect';
+const redirect_uri = 'localhost:3000/login/redirect'
 app.get('/login', function(req, res) {
-	var scopes = 'user-read-private user-read-email';
 	res.redirect('https://accounts.spotify.com/authorize' +
 	  '?response_type=code' +
 	  '&client_id=' + my_client_id +
-	  (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
 	  '&redirect_uri=' + encodeURIComponent(redirect_uri));
 });
 
 app.get('/login/redirect', (req, res) => {
+	console.log(req);
 	res.send(req);
 });
 
