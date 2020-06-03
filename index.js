436e73ec0681412b87e4eca2282f3a3b6ce760b6
Yizhi <lycheesun@hotmail.com>
2020-06-03 15:23:49 -0400

    new update

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/login/redirect', (req, res) => {
 			method: 'GET',
          	headers: { 'Authorization': 'Bearer ' + access_token }
 		};
-		fetch('https://api.spotify.com/v1/me/player/currently-playing', options).then(ret=>{return ret.json()}).then((data)=>console.log(data));
+		fetch('https://api.spotify.com/v1/me/player/currently-playing', options).then(ret=>{return ret.json()}).then((data)=>console.log(data.name));
 	});
 });
 
